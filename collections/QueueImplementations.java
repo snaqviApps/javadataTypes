caaad204e63bd7400e1d4d6c4ca80755828bdbff
Abbas <naqvi.sdsu@gmail.com>
2021-07-25 22:35:02 -0500

    Update QueueImplementations.java

--- collections/QueueImplementations.java
+++ collections/QueueImplementations.java
@@ -1,11 +1,11 @@
-package collections;
+package collection;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class QueueImplementations {
 
-    public static void main(String... args) throws MyNoSuchFieldException {
+//    public static void main(String... args) throws MyNoSuchFieldException {     /** not a very good approach, delegating throw to other object */
+    public static void main(String... args) {
 
         PriorityQueue<Integer> pQueue = new PriorityQueue<>();
         Vector<Integer> vector = new Vector<>();
@@ -15,13 +15,13 @@ public class QueueImplementations {
         }
 
         System.out.println("pQueue-members using plain-object: " + pQueue);
-        System.out.println("vector-members using plain-object: " + vector.parallelStream());
+        System.out.println("vector-members using plain-object: " + Arrays.toString(vector.parallelStream().toArray()));
         System.out.println("===============================\n");
         vector.stream().forEachOrdered(i ->
                 System.out.println("vector-members using forEachOrdered(): " + i.doubleValue()));
         System.out.println("--------------------------------");
         pQueue.forEach(i ->
-                System.out.println("pQueue-members using forEach(): " + i));
+                System.out.print("pQueue-members using forEach(): " + i + "\t"));
         System.out.println("===============================\n");
 
         Iterator<Integer> integerIterator = pQueue.iterator();
@@ -30,19 +30,19 @@ public class QueueImplementations {
         }
 
         // throw exception approach
-        if (pQueue.iterator().hasNext()) {
-            System.out.println("still pQueue has data: " + pQueue.poll());
-        }else {
-            throw new MyNoSuchFieldException("Custom-Exception thrown: pQueue is empty !!! ");
-        }
+//        if (pQueue.iterator().hasNext()) {
+//            System.out.println("still pQueue has data: " + pQueue.poll());
+//        } else {
+//            throw new MyNoSuchFieldException("Custom-Exception thrown: pQueue is empty !!! ");
+//        }
 
         // try - catch approach: Nicer as it doesn't delegate other objects to deal with it
-//        try {
-//            pQueue.remove();                    // program flow does not stop
-//        } catch (NoSuchElementException e) {
-//            System.out.println("inside catch block");
-//            e.printStackTrace();
-//        }
+        try {
+            pQueue.remove();                    // program flow does not stop
+        } catch (NoSuchElementException e) {
+            System.out.println("inside catch block, with thrown exception-info:  " + e);
+            e.printStackTrace();
+        }
 
         System.out.println("post Exception thrown poll() call: " + pQueue.poll());
         System.out.println("post Exception thrown poll() II call: " + pQueue.poll());
@@ -53,6 +53,6 @@ public class QueueImplementations {
 
 class MyNoSuchFieldException extends Throwable {
     public MyNoSuchFieldException(String err) {
-        System.out.println(err);
+        System.out.println("using manual exception-throw approach: " + err);
     }
 }
